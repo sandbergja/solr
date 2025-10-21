19b8180772d135594effd8f4a952e231ee58e885
Jane Sandberg <js7389@princeton.edu>
2025-10-21 07:52:19 -0700

    More idiomatic sealed class usage

--- solr/ui/src/commonTest/kotlin/org/apache/solr/ui/components/indexAndQuery/data/RequestStateTest.kt
+++ solr/ui/src/commonTest/kotlin/org/apache/solr/ui/components/indexAndQuery/data/RequestStateTest.kt
@@ -8,39 +8,39 @@ import kotlinx.io.IOException
 class RequestStateTest {
     @Test
     fun emptyCanIterateButHasNoElementsTest() {
-        val iterator = Empty<CollectionName>().iterator()
+        val iterator = RequestState.Empty<CollectionName>().iterator()
         assertFalse { iterator.hasNext() }
     }
 
     @Test
     fun emptyCanIterateExceptionsButHasNoElementsTest() {
-        val iterator = Empty<CollectionName>().exceptionIterator()
+        val iterator = RequestState.Empty<CollectionName>().exceptionIterator()
         assertFalse { iterator.hasNext() }
     }
 
     @Test
     fun loadingCanIterateButHasNoElementsTest() {
-        val iterator = Loading<CollectionName>().iterator()
+        val iterator = RequestState.Loading<CollectionName>().iterator()
         assertFalse { iterator.hasNext() }
     }
 
     @Test
     fun loadingCanIterateExceptionsButHasNoElementsTest() {
-        val iterator = Loading<CollectionName>().exceptionIterator()
+        val iterator = RequestState.Loading<CollectionName>().exceptionIterator()
         assertFalse { iterator.hasNext() }
     }
 
     @Test
     fun errorCanIterateButHasNoElementsTest() {
         val exception = IOException("I am an exception")
-        val iterator = Error<CollectionName>(exception).iterator()
+        val iterator = RequestState.Error<CollectionName>(exception).iterator()
         assertFalse { iterator.hasNext() }
     }
 
     @Test
     fun errorCanIterateExceptionsButHasNoElementsTest() {
         val exception = IOException("I am an exception")
-        val iterator = Error<CollectionName>(exception).exceptionIterator()
+        val iterator = RequestState.Error<CollectionName>(exception).exceptionIterator()
         assertEquals(iterator.next().message, "I am an exception")
         assertFalse { iterator.hasNext() }
     }
@@ -48,7 +48,7 @@ class RequestStateTest {
     @Test
     fun successCanIterateTest() {
         val list = listOf("catalog-production1", "catalog-production2")
-        val iterator = Success(list).iterator()
+        val iterator = RequestState.Success(list).iterator()
         assertEquals(iterator.next(), "catalog-production1")
         assertEquals(iterator.next(), "catalog-production2")
         assertFalse { iterator.hasNext() }
@@ -56,7 +56,7 @@ class RequestStateTest {
 
     @Test
     fun successCanIterateExceptionsButHasNoElementsTest() {
-        val iterator = Success(listOf("catalog-production1")).exceptionIterator()
+        val iterator = RequestState.Success(listOf("catalog-production1")).exceptionIterator()
         assertFalse { iterator.hasNext() }
     }
 }
