a540387d9e6f22725d8b2b0d012fed98b1b02744
Jane Sandberg <js7389@princeton.edu>
2025-10-21 13:21:42 -0700

    passing provider test

---
+++ solr/ui/src/commonMain/kotlin/org/apache/solr/ui/components/indexAndQuery/integration/Mappers.kt
@@ -0,0 +1,11 @@
+package org.apache.solr.ui.components.indexAndQuery.integration
+
+import org.apache.solr.ui.components.indexAndQuery.IndexAndQueryComponent
+import org.apache.solr.ui.components.indexAndQuery.store.IndexAndQueryStore
+
+internal val indexAndQueryStateToModel: (IndexAndQueryStore.State) -> IndexAndQueryComponent.Model = {
+    IndexAndQueryComponent.Model(
+        selectedCollection = it.selectedCollection,
+        selectedRequestHandler = it.selectedRequestHandler,
+    )
+}
--- solr/ui/src/commonTest/kotlin/org/apache/solr/ui/components/indexAndQuery/store/IndexAndQueryStoreProviderTest.kt
+++ solr/ui/src/commonTest/kotlin/org/apache/solr/ui/components/indexAndQuery/store/IndexAndQueryStoreProviderTest.kt
@@ -1,26 +1,35 @@
 package org.apache.solr.ui.components.indexAndQuery.store
 
+import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
 import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.apache.solr.ui.components.indexAndQuery.data.RequestState
 
 class IndexAndQueryStoreProviderTest {
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun testProvide() = runTest {
         val expectedState = RequestState.Success(listOf("collection1"))
         val client = MockedIndexAndQueryStoreClient(onFetchCollections = { expectedState })
-        launch {
-            // When provide is called, the initial action is already included
-            val store = IndexAndQueryStoreProvider(
-                storeFactory = DefaultStoreFactory(),
-                client = client,
-                mainContext = coroutineContext,
-                ioContext = backgroundScope.coroutineContext,
-            ).provide()
-        }
-        assertTrue { true }
+
+        val store = IndexAndQueryStoreProvider(
+            storeFactory = DefaultStoreFactory(),
+            client = client,
+            mainContext = StandardTestDispatcher(testScheduler),
+            ioContext = StandardTestDispatcher(testScheduler),
+        ).provide()
+
+        assertTrue(store.stateFlow.value.collectionListState is RequestState.Loading<*>)
+
+        advanceUntilIdle()
+
+        assertTrue(store.stateFlow.value.collectionListState is RequestState.Success)
     }
 }
