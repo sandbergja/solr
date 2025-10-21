7b149a69ca7a0addcd59c301d4392e37596f705c
Jane Sandberg <js7389@princeton.edu>
2025-10-21 09:06:22 -0700

    wip

--- solr/ui/src/commonTest/kotlin/org/apache/solr/ui/components/indexAndQuery/store/IndexAndQueryStoreProviderTest.kt
+++ solr/ui/src/commonTest/kotlin/org/apache/solr/ui/components/indexAndQuery/store/IndexAndQueryStoreProviderTest.kt
@@ -1,11 +1,26 @@
 package org.apache.solr.ui.components.indexAndQuery.store
 
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
 import kotlin.test.Test
 import kotlin.test.assertTrue
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
+import org.apache.solr.ui.components.indexAndQuery.data.RequestState
 
 class IndexAndQueryStoreProviderTest {
     @Test
-    fun isGoodTest() {
+    fun testProvide() = runTest {
+        val expectedState = RequestState.Success(listOf("collection1"))
+        val client = MockedIndexAndQueryStoreClient(onFetchCollections = { expectedState })
+        launch {
+            // When provide is called, the initial action is already included
+            val store = IndexAndQueryStoreProvider(
+                storeFactory = DefaultStoreFactory(),
+                client = client,
+                mainContext = coroutineContext,
+                ioContext = backgroundScope.coroutineContext,
+            ).provide()
+        }
         assertTrue { true }
     }
 }
