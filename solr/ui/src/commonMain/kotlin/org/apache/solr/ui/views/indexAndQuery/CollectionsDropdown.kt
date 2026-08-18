557237ca38bf4ae58613074f626dc8342cf5dd2a
Jane Sandberg <js7389@princeton.edu>
2026-08-17 17:54:23 -0700

    Show the collections

--- solr/ui/src/commonMain/kotlin/org/apache/solr/ui/views/indexAndQuery/CollectionsDropdown.kt
+++ solr/ui/src/commonMain/kotlin/org/apache/solr/ui/views/indexAndQuery/CollectionsDropdown.kt
@@ -1,5 +1,6 @@
 package org.apache.solr.ui.views.indexAndQuery
 
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -15,9 +16,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import org.apache.solr.ui.generated.resources.Res
 import org.apache.solr.ui.generated.resources.cd_clear_field
 import org.apache.solr.ui.generated.resources.close
+import org.apache.solr.ui.generated.resources.error_no_collections_available
 import org.apache.solr.ui.generated.resources.nav_configsets
 import org.apache.solr.ui.generated.resources.no_configsets
 import org.jetbrains.compose.resources.painterResource
@@ -74,5 +77,20 @@ fun CollectionsDropdown(
                 )
                 .testTag("collections_dropdown"),
         )
+        ExposedDropdownMenu(
+            modifier = Modifier.testTag("collections_exposed_dropdown_menu"),
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            collections.forEach { collection ->
+                DropdownMenuItem(
+                    text = { Text(collection) },
+                    onClick = {
+                        selectCollection(collection)
+                        expanded = false
+                    },
+                )
+            }
+        }
     }
 }
--- solr/ui/src/commonTest/kotlin/org/apache/solr/ui/views/indexAndQuery/CollectionsDropdownTest.kt
+++ solr/ui/src/commonTest/kotlin/org/apache/solr/ui/views/indexAndQuery/CollectionsDropdownTest.kt
@@ -1,16 +1,22 @@
 package org.apache.solr.ui.views.indexAndQuery
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.v2.runComposeUiTest
 import kotlin.collections.emptyList
 import kotlin.test.Test
+import org.apache.solr.ui.generated.resources.Res
+import org.apache.solr.ui.generated.resources.error_no_collections_available
+import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalTestApi::class)
 class CollectionsDropdownTest {
     @Test
-    fun `GIVEN empty availableConfigsets WHEN dropdown clicked THEN not expanded`() = runComposeUiTest {
+    fun `GIVEN empty available collections WHEN dropdown clicked THEN not expanded`() = runComposeUiTest {
         setContent {
             CollectionsDropdown(
                 selectCollection = {},
@@ -21,5 +27,20 @@ class CollectionsDropdownTest {
 
         onNodeWithTag(testTag = "collections_dropdown").performClick()
         onNodeWithTag(testTag = "collections_exposed_dropdown_menu").assertDoesNotExist()
+    }
+
+    @Test
+    fun `GIVEN several available collections WHEN dropdown clicked THEN expanded`() = runComposeUiTest {
+        setContent {
+            CollectionsDropdown(
+                selectCollection = {},
+                collections = listOf("collection1", "collection2"),
+                selectedCollection = "",
+            )
+        }
+        onNodeWithTag(testTag = "collections_dropdown").performClick()
+        onNodeWithTag(testTag = "collections_exposed_dropdown_menu").assertIsDisplayed()
+        onNodeWithText("collection1").assertIsDisplayed()
+        onNodeWithText("collection2").assertIsDisplayed()
     }
 }
